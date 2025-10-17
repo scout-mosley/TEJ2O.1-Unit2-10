03da68f510f75b9a2e5b6b56ddae6f9b90f47cb8
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-10-17 13:04:51 -0400

    changed code to work properly

--- main.ts
+++ main.ts
@@ -9,27 +9,25 @@
     basic.showIcon(IconNames.Happy)
 
     const lightLevel = input.lightLevel()
-    const strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
-    strip.clear()
+    let neopixelStrip: neopixel.Strip = null
+
 
     //show pixles depending on light levels
 
     if (lightLevel <= 51) {
-        strip.show()
+        neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+        neopixelStrip.setPixelColor(0 + 1 + 2 + 3, neopixel.colors(NeoPixelColors.Red))
     }
 
     if (lightLevel > 52) {
-        strip.range(0, 1).show()
     }
 
     if (lightLevel > 104) {
-        strip.range(0, 2).show()
     }
 
     if (lightLevel > 156) {
-        strip.range(0, 3).show()
     }
 
     if (lightLevel > 208) {
-        strip.range(0, 4).show()
+
     }
