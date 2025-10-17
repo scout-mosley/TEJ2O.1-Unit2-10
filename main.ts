8412a04b9e76a2dfe5586c40288cafb1eec78629
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-10-17 13:10:33 -0400

    clears screen

--- main.ts
+++ main.ts
@@ -6,28 +6,3 @@
 */
 
     basic.clearScreen()
-    basic.showIcon(IconNames.Happy)
-
-    const lightLevel = input.lightLevel()
-    let neopixelStrip: neopixel.Strip = null
-
-
-    //show pixles depending on light levels
-
-    if (lightLevel <= 51) {
-        neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-        neopixelStrip.setPixelColor(0 + 1 + 2 + 3, neopixel.colors(NeoPixelColors.Red))
-    }
-
-    if (lightLevel > 52) {
-    }
-
-    if (lightLevel > 104) {
-    }
-
-    if (lightLevel > 156) {
-    }
-
-    if (lightLevel > 208) {
-
-    }
