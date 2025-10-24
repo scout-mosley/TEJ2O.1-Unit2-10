17abb517bfe3144a28dfee6a799c6fca2aeb0cda
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-10-24 13:25:01 -0400

    reads light level and shows LEDS

--- main.ts
+++ main.ts
@@ -1,8 +1,41 @@
 /* Copyright (c) 2025 MTHS All rights reserved
 *
 * Created by: scout mosley
-* Created on: oct 2025
-* This program read amount of light and shows it in neopixels
+* Created on: Oct 2025
+* This program read amount of light and shows it in neopixels.
 */
 
-    basic.clearScreen()
+let neopixelStrip: neopixel.Strip = null
+let level = input.lightLevel()
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+//clean up
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+
+//reads light level
+if (level <= 51)    
+strip.setPixelColor (0,neopixel.colors(NeoPixelColors.Black))
+    
+if (level > 52)
+strip.setPixelColor (0,neopixel.colors(NeoPixelColors.Red))
+
+if (level > 104)
+strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+
+if (level > 156)
+strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+
+
+if (level > 208)
+strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
